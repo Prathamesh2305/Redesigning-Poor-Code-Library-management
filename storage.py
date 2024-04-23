4eaee2f6d55f950b6620d0b3d9f521e7c477c7e6
Prathamesh Patil <126512220+Prathamesh2305@users.noreply.github.com>
2024-04-23 12:34:26 +0530

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,24 +1,15 @@
 import json
 
 def save_to_file(data, filename):
-    """Save a list of objects to a JSON file.
-
-    Args:
-        data (list): A list of objects (books or users).
-        filename (str): The name of the file to save the data.
-    """
+    """Save a list of objects to a JSON file."""
     with open(filename, 'w') as file:
         json.dump([obj.__dict__ for obj in data], file)
 
 def load_from_file(filename):
-    """Load data from a JSON file and convert it into a list of objects.
+    """Load data from a JSON file and convert it into a list of objects."""
+    from book import Book
+    from user import User  # Importing locally to avoid circular dependency
 
-    Args:
-        filename (str): The name of the file from which to load the data.
-
-    Returns:
-        list: A list of Book or User objects loaded from the file.
-    """
     try:
         with open(filename, 'r') as file:
             data = json.load(file)
